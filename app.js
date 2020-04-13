17ec90d00ef4c32e92fa2a4091ef5e514be4fc46
Hok <hoksuy@gmail.com>
2020-04-13 01:27:11 -0700

    Forward HTTP request to HTTPS for production environment.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var helmet = require('helmet');
+var debug = require('debug')('webapp:server');
 
 var indexRouter = require('./routes/index');
 var app = express();
@@ -12,13 +12,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+if(app.get('env') == 'production')
+  app.all('*', ensureSecure);
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
@@ -36,5 +37,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+function ensureSecure(req, res, next) {
+  if(req.secure) {
+    return next();
+  };
+  res.redirect('https://' + req.hostname + req.url);
+};
 
 module.exports = app;
