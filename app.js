0b46ed2489ee2c3687993469b695ba5b1019c44f
Hok S. Uy <hoksuy@gmail.com>
2020-04-15 16:33:43 -0700

    Re-commit to new repository.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var debug = require('debug')('webapp:server');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var app = express();
@@ -12,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
