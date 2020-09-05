4c49f7726e2cfd31732c4239757e060ffc907f34
Hok S. Uy <hoksuy@gmail.com>
2020-09-04 18:46:12 -0700

    Added aboutme view.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hok S. Uy' });
 });
 
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { title: 'Hok S. Uy - About Me' });
+});
+
 module.exports = router;
